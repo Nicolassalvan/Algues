0a807b449e59c960ca4917f3ee65abfdc0cecaec
ThomasChen <114347777+tcunique@users.noreply.github.com>
2023-01-10 19:28:44 +0100

    animation_avec_agregat

--- video_et_anim.py
+++ video_et_anim.py
@@ -1,42 +1,41 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jan  4 19:08:29 2023
-
 @author: Team Algues
 """
 
-import numpy as np
+# Interface graphique
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation, writers 
-import settings 
-from class_algue import *
+from matplotlib.animation import FuncAnimation, writers, PillowWriter
 
-def video_et_animation(population, rectangle):
+import numpy as np
+
+import settings 
+import class_algue as algue
+
+import deplacement_final as move
+import aggregat as aggr
+
+# ===== CREATION D'UN FICHIER .mp4 ===== #
+
+def video_simulation(population, rectangle):
     """
     Lance la simulation de la population dans la zone délimitée par un rectangle. 
-    Créé un fichier .mp4 à partir du déroulement de la simulation et affiche un plot
-    de la simulation. Utilise la fonction update_frame pour update l'état de la 
-    population.
-
-    Parameters
+    Créé un fichier .mp4 à partir du déroulement de la simulation.
+    Utilise la fonction update_frame pour update l'état de la population.
+    Paramètres
     ----------
     population : class Population
-        population d'algues.
+        Population d'algues.
     rectangle : Box
-        zone de simulation.
-
-    Returns
+        Zone de simulation.
+    Retours
     -------
     None.
-    
     """
     # initialisation du plot
     plt.close('all')
 
-    fig = plt.figure(1)
-    ax = plt.axes()
-
+    fig, ax = plt.subplots()
 
     ax.set_xlim(-5, rectangle.x_max + 5)
     ax.set_ylim(-5, rectangle.x_max + 5)
@@ -44,24 +43,65 @@ def video_et_animation(population, rectangle):
     scat = ax.scatter([], [], s=5)
     
     # initialisation de l'animation
-    animation = FuncAnimation(fig, func=animation_frame, frames=np.arange(0, 1000, 1), fargs=(population, rectangle,scat,), interval=20)
-    plt.show()
+    animation = FuncAnimation(
+        fig, func=animation_frame, frames=np.arange(0, 1000, 1),
+        fargs=(population, rectangle,scat,), interval=50)
 
     # Initialisations du writer
     writer = writers['ffmpeg']
     writer = writer(fps=15, metadata={'artist': 'me'}, bitrate=1800)
 
     animation.save('Les_Belles_Algues.mp4', writer)
-    
-    
-    
 
-def animation_frame(i, population, rect, scat):
+   
+
+# ===== CREATION D'UN GIF ===== #    
+
+def gif_simulation(population, rectangle,aggregat):
+    """
+    Lance la simulation de la population dans la zone délimitée par un rectangle. 
+    Créé un fichier .gif à partir du déroulement de la simulation.
+    Utilise la fonction update_frame pour update l'état de la population.
+    Paramètres
+    ----------
+    population : class Population
+        population d'algues.
+    rectangle : Box
+        zone de simulation.
+    aggregat : Liste des aggrégats
+    Retours
+    -------
+    Aucun.    
+    """
+    # initialisation du plot
+    plt.close('all')
+
+    fig, ax = plt.subplots()
+
+    ax.set_xlim(-5, rectangle.x_max + 5)
+    ax.set_ylim(-5, rectangle.x_max + 5)
+
+    scat = ax.scatter([], [], s=5)
+    
+    # initialisation de l'animation
+    animation = FuncAnimation(
+        fig, func=animation_frame, frames=np.arange(0, 500, 1),
+        fargs=(population, rectangle,aggregat,scat,), interval=50)
+
+    # Initialisations du writer
+    writer = PillowWriter(fps=15, metadata={'artist': 'me'}, bitrate=1800)
+
+    animation.save('Les_Belles_Algues.gif', writer)
+
+
+# ===== FONCTION QUI MET A JOUR LA POPULATION ===== #
+
+def animation_frame(i, population, rect, aggregat,scat):
     """
     Update chaque frame de l'animation en mettant à jour les morts, les reproductions, 
     l'aggrégation, les déplacements et l'âge des algues de la population. 
     
-    Parameters
+    Paramètres
     ------
     i: int
         numero de la frame
@@ -69,36 +109,30 @@ def animation_frame(i, population, rect, scat):
         population d'algues
     rect: class Box
         rectangle dans lequel sont simulées les algues
+    aggregat : Liste des aggrégats
+        
+    Retours
+    ------
+    Aucun
     """
     # On met à jour la population avec les cellules qui meurent 
     #update_death(population, rect, ... )
-    
-    # on met à jour les reproductions des cellules 
-    #update_reprod(population, rect, ...)
-    
+        
     # On met à jour les aggrégats??? 
-    
+    aggr.update_aggregat(population,aggregat)
+    # On met à jour l'âge et la reproduction des cellules
+        
     # On met à jour les déplacements des cellules 
-    
-    # On met à jour l'âge des cellules
-    
-    
+
+    move.deplacement_sans_stresse(population, rect)
+    aggr.deplacement_aggregat(population, rect, aggregat)
     # Update de l'animation avec un scatter
     data = np.c_[population.x, population.y]
     scat.set_offsets(data)
-
+    scat.set_sizes(population.taille)
 
 # === Test === #
-boite = Box(100, 100)
-pop = Population(1000, boite)
-
-video_et_animation(pop, boite)
-
-
-    
-     
-    
-        
-    
-    
-    
+aggregat=[]
+boite = algue.Box(100, 100)
+pop = algue.Population(1000, boite)
+gif_simulation(pop, boite,aggregat)
